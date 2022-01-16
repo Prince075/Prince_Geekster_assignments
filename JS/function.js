dc05a373c315343abf75faea549e1aceeb0756d2
Prince <princesah075@gmail.com>
2022-01-16 17:44:28 +0530

    commit

---
+++ JS/function.js
@@ -0,0 +1,17 @@
+//console.log("hi");
+//function f1(){
+ //   console.log("f1")
+//}
+//function f2(){
+  //  console.log("f2")
+//}
+//f1();
+//f2();
+//console.log("b");
+function add(num1, num2){
+var total=num1 + num2;
+console.log(total);
+console.log(typeof total);
+}
+add(1,2);
+add("2",1);
